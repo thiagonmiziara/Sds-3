9977ff62bade563328f6051db4c7cbd8a6568c6a
thiago miziara <thiagonmiziara@gmail.com>
2021-05-08 10:54:13 -0300

    Pagination

--- frontend/src/components/DataTable/index.tsx
+++ frontend/src/components/DataTable/index.tsx
@@ -2,53 +2,61 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 
 import { SalePage } from "components/types/sale";
+import {Pagination} from "components/Pagination";
 import { BASE_URL } from "utils/requests";
 import { formatLocalDate } from "utils/format";
 
-export function DataTable(){
-const [page,setPage] = useState<SalePage>({
+export function DataTable() {
+  const [page, setPage] = useState<SalePage>({
     first: true,
     last: true,
     number: 0,
     totalElements: 0,
     totalPages: 0,
-});
+  });
+  const[activePage, setActivePage] = useState(0);
 
-useEffect(()=>{
-    axios.get(`${BASE_URL}/sales?page=0&size=10&sort=date,desc`)
-    .then(response =>{
-        setPage(response.data)
-    })
-},[]);
+  useEffect(() => {
+    axios
+      .get(`${BASE_URL}/sales?page=${activePage}&size=20&sort=date,desc`)
+      .then((response) => {
+        setPage(response.data);
+      });
+  }, [activePage]);
 
+  function changePage(index:number){
+    setActivePage(index)
+  }
 
-
-  return(
-    <div className="table-responsive">
-    <table className="table table-striped table-sm">
-        <thead>
+  return (
+    <>
+    <Pagination page={page} onPageChange={changePage}/>
+      <div className="table-responsive">
+        <table className="table table-striped table-sm">
+          <thead>
             <tr>
-                <th>Data</th>
-                <th>Vendedor</th>
-                <th>Clientes visitados</th>
-                <th>Negócios fechados</th>
-                <th>Valor</th>
+              <th>Data</th>
+              <th>Vendedor</th>
+              <th>Clientes visitados</th>
+              <th>Negócios fechados</th>
+              <th>Valor</th>
             </tr>
-        </thead>
-        <tbody>
-            {page.content?.map(sale =>{
-                return(
-                    <tr key={sale.id}>
-                    <td>{formatLocalDate(sale.date, "dd/MM/yyyy")}</td>
-                    <td>{sale.seller.name}</td>
-                    <td>{sale.visited}</td>
-                    <td>{sale.deals}</td>
-                    <td>{sale.amount.toFixed(2)}</td>
-                </tr>   
-                )
+          </thead>
+          <tbody>
+            {page.content?.map((sale) => {
+              return (
+                <tr key={sale.id}>
+                  <td>{formatLocalDate(sale.date, "dd/MM/yyyy")}</td>
+                  <td>{sale.seller.name}</td>
+                  <td>{sale.visited}</td>
+                  <td>{sale.deals}</td>
+                  <td>{sale.amount.toFixed(2)}</td>
+                </tr>
+              );
             })}
-        </tbody>
-    </table>
-</div>
+          </tbody>
+        </table>
+      </div>
+    </>
   );
 }
---
+++ frontend/src/components/Pagination/index.tsx
@@ -0,0 +1,36 @@
+import { SalePage } from "../types/sale";
+
+type PaginationProps = {
+  page: SalePage;
+  onPageChange: Function;
+};
+
+export function Pagination({ page, onPageChange }: PaginationProps) {
+  return (
+    <div className="row d-flex justify-content-center">
+      <nav>
+        <ul className="pagination">
+          <li className={`page-item ${page.first ? "disabled" : ""}`}>
+            <button
+              className="page-link"
+              onClick={() => onPageChange(page.number - 1)}
+            >
+              Anterior
+            </button>
+          </li>
+          <li className="page-item">
+            <span className="page-link">{page.number + 1}</span>
+          </li>
+          <li className={`page-item ${page.last ? "disabled" : ""}`}>
+            <button
+              className="page-link"
+              onClick={() => onPageChange(page.number + 1)}
+            >
+              Próxima
+            </button>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
+}
